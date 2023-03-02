221bc313c80ef6550d39e8d8bfc8cfec012f3176
Lucas Teixeira <l.teixeira9953@gmail.com>
2023-03-02 17:41:39 -0300

    Estilizando o programa
    fazendo o CSS

--- index.php
+++ index.php
@@ -8,13 +8,9 @@ include("processa_pesquisa.php");
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     <title>Calculadora Farm</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
     <head>
@@ -22,8 +18,10 @@ include("processa_pesquisa.php");
         <hr />
     </head>
     <form action="index.php" method="POST">
+   
     <label>Nome Cultura:</label>
 <input type="text" name="f_nome_cultura" placeholder="Digite a cultura" value="<?php echo isset($nome_cultura) ? $nome_cultura : ''; ?>" id="f2_nome_cultura">
+
 <input type="submit" value="Pesquisar">
         
 
@@ -43,7 +41,8 @@ include("processa_pesquisa.php");
         
 
         <br><br>
-        <input class="btn-bd-primary" type="submit" value="calcular" id="calcular">
+        <input type="submit" value="Calcular" id="calcular">
+        <br><br>
         <input type="submit" value="Limpar Pesquisa" id="limpar">
         
         </form>
